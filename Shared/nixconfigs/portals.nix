47582661e38200635a0fa89a2e8af41547fb733c
rPlakama <rPlakama@proton.me>
2025-11-13 22:14:11 -0300

    ..

--- Shared/nixconfigs/portals.nix
+++ Shared/nixconfigs/portals.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }: {
   xdg.portal = {
+
     enable = true;
     config = {
+
       niri."org.freedesktop.impl.portal.FileChooser" = "gtk";
       niri.default = "gnome";
       common.default = "gnome";
       obs.default = "gnome";
     };
     extraPortals = with pkgs; [
+
       xdg-desktop-portal-gnome
       xdg-desktop-portal-gtk
+
     ];
   };
 }
