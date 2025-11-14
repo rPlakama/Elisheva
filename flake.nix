dc57e66057dc9f6930dc9c8908ed7d6b7fbaac83
rPlakama <rPlakama@proton.me>
2025-11-13 21:59:10 -0300

    update da estrutura do flake para suportar o DMS

--- flake.nix
+++ flake.nix
@@ -15,16 +15,11 @@
       url = "github:AvengeMedia/dgop";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dms-cli = {
-      url = "github:AvengeMedia/danklinux";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         dgop.follows = "dgop";
-        dms-cli.follows = "dms-cli";
       };
     };
     niri = {
