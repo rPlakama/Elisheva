4572fbb2bfb3e2690aab35494f3d217499b0e9b6
rPlakama <rPlakama@proton.me>
2025-11-19 14:44:36 -0300

    Fixed!

---
+++ Shared/programs/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ./neovim.nix
+    ./localsend.nix
+    ./starship.nix
+    ./fish.nix
+    ./steam.nix
+  ];
+}
---
+++ Shared/programs/fish.nix
@@ -0,0 +1,16 @@
+{...}: {
+  programs = {
+    starship.transientPrompt.enable = true;
+    zoxide = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    fish = {
+      enable = true;
+      shellInit = ''
+        set -g fish_key_bindings fish_vi_key_bindings
+        set -g fish_greeting ""
+      '';
+    };
+  };
+}
