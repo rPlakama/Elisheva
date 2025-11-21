97d9f3cb2a964848614b291d8e36ea70c51323f4
rPlakama <rPlakama@proton.me>
2025-11-20 22:11:30 -0300

    Check

--- Elisheva/config/niri/niri-window_rules.nix
+++ Elisheva/config/niri/niri-window_rules.nix
@@ -8,10 +8,10 @@
       {
         clip-to-geometry = true;
         geometry-corner-radius = {
-          top-left = 6.0;
-          top-right = 6.0;
-          bottom-left = 6.0;
-          bottom-right = 6.0;
+          top-left = 2.0;
+          top-right = 2.0;
+          bottom-left = 2.0;
+          bottom-right = 2.0;
         };
       }
       {
