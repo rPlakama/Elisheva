1b8691f90144d27dc74dce5cbca529c0d9383e18
rPlakama <rPlakama@proton.me>
2025-11-23 21:55:59 -0300

    From 535, its default now

--- Centuria/hardware/nvidia.nix
+++ Centuria/hardware/nvidia.nix
@@ -11,8 +11,6 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    modesetting.enable = true;
-
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
