e3c5a5e4b9784a5f424849050fd4f8bb110e633d
rPlakama <rPlakama@proton.me>
2025-11-27 14:08:48 -0300

    Normal kernel for Elisheva

--- Elisheva/boot/paramenters.nix
+++ Elisheva/boot/paramenters.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}:
 
 {
-    boot.kernelPackages = pkgs.linuxPackages_cachyos;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
     boot.kernelParams = [
     "rcutree.enable_rcu_lazy=1"
     "video=eDP-1:1920x1080@75"
