f1eca68697828f38e4a0cbc3e7c2faf28ce6c8ad
rPlakama <rPlakama@proton.me>
2025-11-30 16:03:44 -0300

    Maybe now?

--- Shared/boot/nixos-init.nix
+++ Shared/boot/nixos-init.nix
@@ -2,5 +2,4 @@
   system.nixos-init.enable = true;
   boot.initrd.systemd.enable = true;
   system.etc.overlay.enable = true;
-  services.userborn.enable = true;
 }
--- Shared/nixconfigs/users.nix
+++ Shared/nixconfigs/users.nix
@@ -1,4 +1,6 @@
 {pkgs, ...}: {
+  users.mutableUsers = false;
+  systemd.sysusers.enable = true;
   users.users.rplakama = {
     isNormalUser = true;
     description = "I like Shark Girls.";
