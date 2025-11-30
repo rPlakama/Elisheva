2a4706c5024239e3e18c2e3fe6f29632033d8d25
rPlakama <rPlakama@proton.me>
2025-11-30 18:23:11 -0300

    Man idgf

--- Shared/nixconfigs/users.nix
+++ Shared/nixconfigs/users.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: {
   users.mutableUsers = false;
+  services.userborn.enable = true;
   users.users.rplakama = {
     isNormalUser = true;
-    description = "I like Shark Girls.";
-    hashedPasswordFile = "./rplakama.txt";
+    description = "Hi.";
+    hashedPassword = "$y$j9T$/IUBMaO9Dx6HKTarmsvjq1$y8CWBVWIxMYCkMYLOKrhSSnW2/ESE1JeC8BIXAMwtj1";
     extraGroups = [
       "wheel"
       "networkmanager"
