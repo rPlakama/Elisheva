ba4045ad83b0a560adb1037743d9d45fe4d3c340
rPlakama <rPlakama@proton.me>
2025-12-02 13:57:50 -0300

    Fixed small typo

--- Config/programs/terminal.nix
+++ Config/programs/terminal.nix
@@ -8,7 +8,10 @@
     };
     fish = {
       enable = true;
-      shellInit = ''set -g fish_key_bindings fish_vi_key_bindings set -g fish_greeting "" '';
+      shellInit = ''
+        set -g fish_key_bindings fish_vi_key_bindings
+        set -g fish_greeting ""
+      '';
     };
   };
 }
