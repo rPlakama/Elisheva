0759611545fe9aa270a46cb1a52b79e9a194683f
rPlakama <rPlakama@proton.me>
2025-12-01 22:11:34 -0300

    Finished final touchs on flood + tranmission

--- Shared/services/torrent.nix
+++ Shared/services/torrent.nix
@@ -1,41 +1,19 @@
 {
-  config,
   pkgs,
   ...
-}: let
-  peer-port = 51413;
-  web-port = 3000;
-  downloadDir = "${config.users.users.rtorrent.home}/Torrents";
-in {
-  users.users.rtorrent = {
-    isSystemUser = true;
-    group = "rtorrent";
-    home = "/var/lib/rtorrent";
-    createHome = true;
+}: {
+    services.transmission = {
+      enable = true;
+
+      package = with pkgs; transmission_4;
+      webHome = with pkgs; flood-for-transmission;
+
+      user = "rplakama";
+      group = "users";
+      settings = {
+	download-dir = "/home/rplakama/Torrents/";
+	rpc-port = 9091;
+	rpc-bind-address = "0.0.0.0";
+    };
   };
-
-  users.groups.rtorrent = {};
-
-  systemd.tmpfiles.rules = [
-    "d ${downloadDir} 0755 rtorrent rtorrent -"
-  ];
-
-  services.rtorrent = {
-    enable = true;
-    port = peer-port;
-    package = pkgs.rtorrent;
-    openFirewall = true;
-    downloadDir = downloadDir;
-  };
-
-  services.flood = {
-    enable = true;
-    port = web-port;
-    openFirewall = true;
-    extraArgs = ["--rtsocket=${config.services.rtorrent.rpcSocket}"];
-  };
-
-  systemd.services.flood.serviceConfig.SupplementaryGroups = [
-    config.services.rtorrent.group
-  ];
 }
