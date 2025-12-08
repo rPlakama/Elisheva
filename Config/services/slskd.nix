52a0d8fea8be6686b09e0f70cda3a9ca1a502cbd
rPlakama <rPlakama@proton.me>
2025-12-07 22:23:45 -0300

    Hopefully fully configured now

--- Config/services/slskd.nix
+++ Config/services/slskd.nix
@@ -4,5 +4,15 @@
     enable = true;
     domain = "slskd.nix.com";
     openFirewall = true;
+    user = "rplakama";
+    group = "users";
+    environmentFile = "/home/rplakama/Dropbox/env.yaml";
+    settings = {
+      shares.directories = ["/home/rplakama/Music/"];
+      directories = {
+        downloads = "/home/rplakama/Music/";
+        incomplete = "/home/rplakama/Downloads/Soulseek/Incomplete";
+      };
+    };
   };
 }
