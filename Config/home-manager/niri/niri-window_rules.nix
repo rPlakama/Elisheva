a096bd40a5516df12f8494b9041ca1aef47bc68c
rPlakama <rPlakama@proton.me>
2025-12-10 15:49:05 -0300

    new border radius

--- Config/home-manager/niri/niri-window_rules.nix
+++ Config/home-manager/niri/niri-window_rules.nix
@@ -8,10 +8,10 @@
       {
         clip-to-geometry = true;
         geometry-corner-radius = {
-          top-left = 0.0;
-          top-right = 0.0;
-          bottom-left = 0.0;
-          bottom-right = 0.0;
+          top-left = 4.0;
+          top-right = 4.0;
+          bottom-left = 4.0;
+          bottom-right = 4.0;
         };
       }
       {
