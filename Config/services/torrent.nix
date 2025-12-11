e3e5df9d98358728c4932a7deba4c957741693ec
rPlakama <rPlakama@proton.me>
2025-12-10 23:04:08 -0300

    Small update on torrenting

--- Config/services/torrent.nix
+++ Config/services/torrent.nix
@@ -2,6 +2,7 @@
 {
   services.transmission = {
     enable = true;
+    openFirewall = true;
 
     package = with pkgs; transmission_4;
     webHome = with pkgs; flood-for-transmission;
@@ -10,6 +11,7 @@
     group = "users";
     settings = {
       download-dir = "/home/rplakama/Torrents/";
+      peer-port = 51413;
       rpc-port = 9091;
       rpc-bind-address = "0.0.0.0";
     };
