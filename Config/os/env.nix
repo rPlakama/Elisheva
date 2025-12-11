55c258242b289ad7f5175d105fce7d4eb733a576
rPlakama <rPlakama@proton.me>
2025-12-11 13:13:37 -0300

    Some aliases for my confort.
    Added nvim aliase, added rebuild alises with HOTNAME argument to make it easier alr

--- Config/os/env.nix
+++ Config/os/env.nix
@@ -4,6 +4,11 @@
     sessionVariables.NIXOS_OZONE_WL = "1";
     shellAliases = {
       develop = "nix develop ./.nix-develop-cache-1-link";
+      sfs = "echo 'Rebuilding system as SWITCH'
+      sudo nixos-rebuild switch --flake .#HOSTNAME";
+      sfb = "echo 'Rebulding system as BOOT'
+      sudo nixos-rebuild boot --flake .#HOSTNAME";
+      nv = "nvim";
       "..." = "cd ../../";
       "...." = "cd ../../../";
     };
