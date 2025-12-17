20f188c2bf97b8733b7cecd408a6c400097eef81
rPlakama <rPlakama@proton.me>
2025-12-17 10:31:58 -0300

    Mesa for elisheva

--- Config/packages/system_packages.nix
+++ Config/packages/system_packages.nix
@@ -1,19 +1,26 @@
 {
+  config,
+  lib,
   pkgs,
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    bat
-    fd
-    ripgrep
-    p7zip
-    yazi
-    ripdrag
-    cliphist
-    wl-clipboard
-    wl-mirror
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      bat
+      fd
+      ripgrep
+      p7zip
+      yazi
+      ripdrag
+      cliphist
+      wl-clipboard
+      wl-mirror
+    ]
+    ++ lib.optionals (config.networking.hostName == "Elisheva") [
+      mesa
+    ];
   fonts.packages = with pkgs; [
     nerd-fonts.caskaydia-cove
     montserrat
