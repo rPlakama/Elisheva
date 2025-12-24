1f75dd6741ca96d1ae1a476b6cb3f85aca25cc60
rPlakama <rPlakama@proton.me>
2025-12-24 17:44:49 -0300

    Incr

--- Config/os/security.nix
+++ Config/os/security.nix
@@ -6,7 +6,6 @@
   security.sudo-rs.enable = true;
   programs.dank-material-shell.greeter = {
     compositor.name = "niri";
-    quickshell.package = inputs.quickshell.packages.x86_64-linux.default;
     enable = true;
     configHome = "/home/rplakama/";
   };
