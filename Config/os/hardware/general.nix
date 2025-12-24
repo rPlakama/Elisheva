8d7a3b8f6cbdeb95ef3bcac98f85066da27edfdb
rPlakama <rPlakama@proton.me>
2025-12-24 18:04:37 -0300

    changes nvidia

--- Config/os/hardware/general.nix
+++ Config/os/hardware/general.nix
@@ -16,7 +16,7 @@ lib.mkMerge [
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   })
 
