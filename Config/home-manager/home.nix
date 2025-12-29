880570d163b0681977ccf19fa88a1eab34546567
rPlakama <rPlakama@proton.me>
2025-12-29 09:26:07 -0300

    f1

--- Config/home-manager/home.nix
+++ Config/home-manager/home.nix
@@ -4,5 +4,13 @@
     ./default.nix
   ];
 
-  home.stateVersion = "25.05";
+  home = {
+    stateVersion = "25.05";
+    pointerCursor = {
+      package = pkgs.volantes-cursors;
+      name = "volantes_light_cursors";
+      size = 24;
+      x11.enable = true;
+    };
+  };
 }
--- Config/home-manager/media.nix
+++ Config/home-manager/media.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
+let
+  myEQ = "@lavfi:lavfi=[volume=volume=-3.2dB,equalizer=f=72:width_type=q:width=1.2:g=1.4,equalizer=f=120:width_type=q:width=2.0:g=0.7,equalizer=f=180:width_type=q:width=1.7:g=-1.6,equalizer=f=360:width_type=q:width=2.0:g=1.3,equalizer=f=490:width_type=q:width=0.7:g=2.8,equalizer=f=510:width_type=q:width=2.0:g=-0.8,equalizer=f=1700:width_type=q:width=2.0:g=-2.1,equalizer=f=7000:width_type=q:width=3.0:g=-2.0]";
+in
 {
   programs = {
     mpv = {
@@ -6,6 +9,10 @@
       scripts = with pkgs; [ mpvScripts.mpris ];
       config = {
         save-position-on-quit = true;
+        af = myEQ;
+      };
+      bindings = {
+        "CTRL+e" = "af toggle @lavfi";
       };
     };
   };
