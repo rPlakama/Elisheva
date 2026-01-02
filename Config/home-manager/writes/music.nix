3e88cf2833b699fc34d409f9c36169f773c142ec
rPlakama <rPlakama@proton.me>
2026-01-01 22:36:32 -0300

    Path fixes

--- Config/home-manager/writes/music.nix
+++ Config/home-manager/writes/music.nix
@@ -5,29 +5,30 @@
   home.file."Music/music.sh" = {
     executable = true;
     text = ''
-          #!/usr/bin/env fish
+      #!/usr/bin/env fish
 
-      set -x FZF_DEFAULT_OPTS "--layout=reverse --border"
+        cd ~/Music;
+        set -x FZF_DEFAULT_OPTS "--layout=reverse --border"
 
-      while true
-          set selected (fd . --exact-depth 2 --type d | fzf \
-              -m \
-              --preview 'ls -1 {}' \
-              --prompt 'Play > ')
+        while true
+            set selected (fd . --exact-depth 2 --type d | fzf \
+                -m \
+                --preview 'ls -1 {}' \
+                --prompt 'Play > ')
 
-          if test -z "$selected"
-              break
-          end
+            if test -z "$selected"
+                break
+            end
 
-          clear
+            clear
 
-          mpv \
-      	--no-save-position-on-quit \
-              --term-osd-bar \
-              --gapless-audio=yes \
-              --replaygain=album \
-              $selected
-      end
+            mpv \
+        	--no-save-position-on-quit \
+                --term-osd-bar \
+                --gapless-audio=yes \
+                --replaygain=album \
+                $selected
+        end
 
     '';
   };
