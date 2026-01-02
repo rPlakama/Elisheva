7155b843f8753e1068bf13eca987793285c72ef0
rPlakama <rPlakama@proton.me>
2026-01-01 22:27:59 -0300

    Music I guess.

--- Config/home-manager/writes/music.nix
+++ Config/home-manager/writes/music.nix
@@ -5,17 +5,15 @@
   home.file."Music/music.sh" = {
     executable = true;
     text = ''
-      #!/usr/bin/env fish
-      cd ~/Music || exit
+          #!/usr/bin/env fish
 
-      set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
+      set -x FZF_DEFAULT_OPTS "--layout=reverse --border"
 
       while true
           set selected (fd . --exact-depth 2 --type d | fzf \
               -m \
               --preview 'ls -1 {}' \
-              --header 'TAB to queue, ENTER to play' \
-              --prompt 'Album > ')
+              --prompt 'Play > ')
 
           if test -z "$selected"
               break
@@ -24,13 +22,13 @@
           clear
 
           mpv \
-              --no-video \
-              --no-save-position-on-quit \
+      	--no-save-position-on-quit \
               --term-osd-bar \
-              --gapless-audio=weak \
+              --gapless-audio=yes \
               --replaygain=album \
               $selected
       end
+
     '';
   };
 }
