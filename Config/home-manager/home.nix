7196446b975dc78efe85954a1b69a4df2d6cfcaf
rPlakama <rPlakama@proton.me>
2026-01-13 19:16:47 -0300

    Eq

--- Config/home-manager/home.nix
+++ Config/home-manager/home.nix
@@ -6,11 +6,5 @@
 
   home = {
     stateVersion = "25.05";
-    pointerCursor = {
-      package = pkgs.volantes-cursors;
-      name = "volantes_light_cursors";
-      size = 24;
-      x11.enable = true;
-    };
   };
 }
--- Config/home-manager/media.nix
+++ Config/home-manager/media.nix
@@ -1,11 +1,26 @@
 { pkgs, ... }:
+let
+  # PEQ Profile
+  # Filters:
+  # 3: PK Fc 1900 Hz Gain -2.2 dB Q 1.800
+  # 4: PK Fc 3100 Hz Gain 1.0 dB Q 2.000
+  # 5: PK Fc 6900 Hz Gain 1.4 dB Q 2.000
+  # 6: PK Fc 7600 Hz Gain -3.0 dB Q 1.700
+  targetEq = "lavfi=[volume=volume=-4dB,equalizer=f=1900:width_type=q:w=1.8:g=-2.2,equalizer=f=3100:width_type=q:w=2.0:g=1.0,equalizer=f=6900:width_type=q:w=2.0:g=1.4,equalizer=f=7600:width_type=q:w=1.7:g=-3.0]";
+in
 {
   programs = {
     mpv = {
       enable = true;
       scripts = with pkgs; [ mpvScripts.mpris ];
+
       config = {
         save-position-on-quit = true;
+        af = "${targetEq}";
+      };
+
+      bindings = {
+        "Ctrl+e" = "no-osd af toggle \"${targetEq}\"; show-text \"EQ Toggled\"";
       };
     };
   };
