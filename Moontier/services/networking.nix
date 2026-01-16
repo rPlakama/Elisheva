e654a825f06dec2c88b28aed974e517d07393f2d
rPlakama <rPlakama@proton.me>
2026-01-16 15:00:49 -0300

    Revert on deluge

--- Moontier/services/networking.nix
+++ Moontier/services/networking.nix
@@ -1,14 +1,15 @@
 { ... }:
 {
   networking = {
-    firewall.allowedUDPPorts = [ 41641 ];
+    firewall.allowedUDPPorts = [
+      41641
+    ];
     firewall.allowedTCPPorts = [
       80
       443
       8085
       8096
       5030
-      # Torrent
       58846
       9091
     ];
--- Moontier/services/torrent.nix
+++ Moontier/services/torrent.nix
@@ -1,14 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
+
 {
-  services.deluge = {
+  services.transmission = {
     enable = true;
+    openFirewall = true;
+    openRPCPort = true;
     group = "users";
+    package = pkgs.transmission_4;
+    webHome = pkgs.flood-for-transmission;
 
-    web = {
-      enable = true;
-      openFirewall = true;
-      port = 9091;
+    settings = {
+      download-dir = "/mnt/secondary/torrent";
+      incomplete-dir-enabled = true;
+      incomplete-dir = "/mnt/secondary/torrent/.incomplete";
+      rpc-whitelist-enabled = false;
+      speed-limit-up-enabled = false;
+      rpc-bind-address = "0.0.0.0";
+      rpc-port = 9091;
     };
-
   };
+
 }
