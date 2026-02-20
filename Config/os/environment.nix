54b98bca6076ccc963ffb76403f05544183604c6
rPlakama <rPlakama@proton.me>
2026-02-20 16:03:42 -0300

    Removed aliases.

--- Config/os/environment.nix
+++ Config/os/environment.nix
@@ -6,13 +6,7 @@
       MANPAGER = "nvim +Man!";
     };
     shellAliases = {
-
-      sfs = "echo -e 'Rebuild Type: \033[32m SWITCH \033[0m'; sudo nixos-rebuild switch --flake .#$HOSTNAME";
-      sfb = "echo -e 'Rebuild Type: \033[32m BOOT \033[0m'; sudo nixos-rebuild boot --flake .#$HOSTNAME";
-      sft = "echo -e 'Rebuild Type: \033[32m TEST \033[0m'; sudo nixos-rebuild test --flake .#$HOSTNAME";
-
       nv = "nvim";
-
       "..." = "cd ../../";
       "...." = "cd ../../../";
     };
