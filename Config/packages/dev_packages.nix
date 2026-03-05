0ef053589f08b2b0a7fffdd9c872e7fce17d18ea
rPlakama <rPlakama@proton.me>
2026-03-05 15:29:49 -0300

    Lazy-lsp do it for me

--- Config/packages/dev_packages.nix
+++ Config/packages/dev_packages.nix
@@ -9,16 +9,11 @@
     with pkgs;
     [
       git
-      nixd
     ]
     ++ lib.optionals isDesktop [
       jq
       typst
-      lua-language-server
-      nixfmt-rfc-style
-      fish-lsp
       nodejs
       android-studio
-      tinymist
     ];
 }
