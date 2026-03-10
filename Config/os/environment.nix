45c5fe0de375bb5ed8eba0f7acd8def891c9f279
rPlakama <rPlakama@proton.me>
2026-03-10 13:55:39 -0300

    For IDE

--- Config/os/environment.nix
+++ Config/os/environment.nix
@@ -4,6 +4,8 @@
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
       MANPAGER = "nvim +Man!";
+      _JAVA_AWT_WM_NONREPARENTING = 1;
+      STUDIO_VM_OPTIONS = "-Dwayland.enabled=true";
     };
     shellAliases = {
       "..." = "cd ../../";
