a117f830a33b71b0d19a41e1b22665ea72da9f1b
rPlakama <rPlakama@proton.me>
2026-04-12 11:54:48 -0300

    Fix scope in executable service gnome on Centuria host

--- executables/Centuria-executables.nix
+++ executables/Centuria-executables.nix
@@ -4,11 +4,11 @@
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
-  };
-  gnome = {
-    core-apps.enable = false;
-    core-developer-tools.enable = false;
-    games.enable = false;
+    gnome = {
+      core-apps.enable = false;
+      core-developer-tools.enable = false;
+      games.enable = false;
+    };
   };
   programs = {
     steam = {
