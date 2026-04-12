058cb8a2c73d09d20f488de97924076b319db626
rPlakama <rPlakama@proton.me>
2026-04-12 00:02:47 -0300

    small fix on syntax

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,16 +9,12 @@
     ./programs.nix
     ./neovim/neovim.nix
   ]
-  ++
-    lib.optionals isDesktop
-      [
-        ./desktop-programs.nix
-      ]
-      lib.optionals
-      isElisheva
-      [
-        ./desktop-niri.nix
-      ];
+  ++ lib.optionals isDesktop [
+    ./desktop-programs.nix
+  ]
+  ++ lib.optionals isElisheva [
+    ./desktop-niri.nix
+  ];
 
   home.stateVersion = "26.05";
 }
