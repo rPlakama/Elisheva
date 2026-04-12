8143b2675f9e86f56788984844c730e84ffe1313
rPlakama <rPlakama@proton.me>
2026-04-11 23:27:34 -0300

    Exclude certain gnome packages

--- pkgs/Centuria-pkgs.nix
+++ pkgs/Centuria-pkgs.nix
@@ -2,6 +2,25 @@
 
 {
 
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      baobab # disk usage analyzer
+      geary # email reader
+      gnome-tour # GNOME tour app
+      yelp # help viewer
+    ])
+    ++ (with pkgs.gnome; [
+      # Packages under the pkgs.gnome namespace
+      gnome-calculator
+      gnome-calendar
+      gnome-characters
+      gnome-clocks
+      gnome-contacts
+      gnome-font-viewer
+      gnome-logs
+      gnome-maps
+      gnome-weather
+    ]);
   environment.systemPackages = with pkgs; [
     btop-cuda
     bottles
