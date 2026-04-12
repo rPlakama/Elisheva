66bb527c8827cd9c045ceeb5b761e45919afce88
rPlakama <rPlakama@proton.me>
2026-04-12 00:00:20 -0300

    Niri config _as_ Elisheva only.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,13 +1,24 @@
-{ lib, isDesktop, ... }:
+{
+  isElisheva,
+  lib,
+  isDesktop,
+  ...
+}:
 {
   imports = [
     ./programs.nix
     ./neovim/neovim.nix
   ]
-  ++ lib.optionals isDesktop [
-    ./desktop-programs.nix
-    ./desktop-niri.nix
-  ];
+  ++
+    lib.optionals isDesktop
+      [
+        ./desktop-programs.nix
+      ]
+      lib.optionals
+      isElisheva
+      [
+        ./desktop-niri.nix
+      ];
 
   home.stateVersion = "26.05";
 }
