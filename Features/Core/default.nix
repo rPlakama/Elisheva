2fb4f168ad8baececc99551167039ba190e1d37b
rPlakama <rPlakama@proton.me>
2026-04-14 15:11:40 -0300

    Core services added.

--- Features/Core/default.nix
+++ Features/Core/default.nix
@@ -9,7 +9,7 @@
   options.core.features.core.enable = lib.mkOption {
     type = lib.types.bool;
     default = true;
-    description = "Essential system packages";
+    description = "Essential system segments";
   };
 
   config = lib.mkIf config.core.features.core.enable {
@@ -28,5 +28,37 @@
       montserrat
       arkpandora_ttf
     ];
+    services = {
+
+      upower.enable = true;
+      bpftune.enable = true;
+      devmon.enable = true;
+      udisks2.enable = true;
+      resolved.enable = true;
+
+      tailscale = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
+
+      openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = true;
+          PermitRootLogin = "no";
+        };
+      };
+
+      scx = {
+        enable = true;
+        scheduler = "scx_lavd";
+      };
+    };
   };
 }
---
+++ Features/Core/native_services.nix
@@ -0,0 +1,36 @@
+{ ... }:
+{
+
+  services = {
+
+    upower.enable = true;
+    bpftune.enable = true;
+    devmon.enable = true;
+    udisks2.enable = true;
+    resolved.enable = true;
+
+    tailscale = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = true;
+        PermitRootLogin = "no";
+      };
+    };
+
+    scx = {
+      enable = true;
+      scheduler = "scx_lavd";
+    };
+  };
+}
