4a5da7897c0bfc9b3c44b7d8487a33e2ebdc8196
rPlakama <rPlakama@proton.me>
2026-04-15 23:08:59 +0000

    More hz for Elisheva

--- Hosts/Elisheva/default.nix
+++ Hosts/Elisheva/default.nix
@@ -9,6 +9,9 @@
   environment.systemPackages = with pkgs; [
     moonlight-qt # << Victim of evil sunshine
   ];
+  boot.kernelParams = [
+    "video=eDP-1:1920x1080@72" # <-- Shitty Screen
+  ];
 
   core.user = "rplakama";
   optionals.features = {
