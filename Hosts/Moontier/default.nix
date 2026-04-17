a15c2238ea8f932c03b19f17cf6412f21523f0b1
rPlakama <rPlakama@proton.me>
2026-04-17 12:36:26 -0300

    Suwayomi enabled as scraper

--- Hosts/Moontier/default.nix
+++ Hosts/Moontier/default.nix
@@ -12,7 +12,7 @@
     niri.enable = false;
     neovim.enable = true;
     graphicalPkgs.enable = false;
-    suwayomi.enable = false;
+    suwayomi.enable = true;
     komga.enable = true;
     pi-hole.enable = true;
     nzbget.enable = true;
