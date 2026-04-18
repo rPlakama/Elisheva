24559e27d4af7e4120db8700cac872ef44bb4640
rPlakama <rPlakama@proton.me>
2026-04-18 08:49:11 -0300

    Testing of nginx

--- Features/Nginx/default.nix
+++ Features/Nginx/default.nix
@@ -11,12 +11,54 @@ in
 {
   options.optionals.features.nginx.enable = lib.mkOption {
     type = lib.types.bool;
-    description = "nginx Configuration";
+    description = "Nginx Configuration with SOPS SSL";
     default = false;
   };
+
   config = lib.mkIf cfg.enable {
+
+    sops.secrets."nginx/moontier_key" = {
+      owner = "nginx";
+      group = "nginx";
+      mode = "0400";
+    };
+
+    sops.secrets."nginx/moontier_crt" = {
+      owner = "nginx";
+      group = "nginx";
+      mode = "0444";
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+
     services.nginx = {
       enable = true;
+
+      recommendedGzipSettings = true;
+      recommendedOptimisation = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts."moontier.local" = {
+        forceSSL = true;
+
+        sslCertificate = config.sops.secrets."nginx/moontier_crt".path;
+        sslCertificateKey = config.sops.secrets."nginx/moontier_key".path;
+
+        extraConfig = ''
+          allow 192.168.1.0/24;
+          allow 127.0.0.1;
+          deny all;
+        '';
+
+        locations."/evil" = {
+          proxyPass = "http://192.168.1.106:8096";
+          proxyWebsockets = true;
+        };
+      };
     };
   };
 }
