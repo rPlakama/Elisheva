0ec5ffc5b6519f8a07bb8f593fd20ff2c64df511
rPlakama <rPlakama@proton.me>
2026-05-03 11:48:05 -0300

    Removed ununsed modules, updated enables for Moontier host.

--- Hosts/Moontier/default.nix
+++ Hosts/Moontier/default.nix
@@ -16,11 +16,8 @@
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-server-lto;
   core.user = "rplakama";
   optionals.features = {
-    niri.enable = false;
     neovim.enable = true;
     graphicalPkgs.enable = false;
-    suwayomi.enable = true;
-    komga.enable = false;
     kavita.enable = true;
     pi-hole.enable = true;
     nzbget.enable = true;
@@ -32,7 +29,10 @@
     Samba.enable = true;
     galleryDl = {
       enable = true;
-      urls = [ "https://mangafire.to/manga/madan-no-ichii.w5x17" ];
+      urls = [
+        "https://mangafire.to/manga/madan-no-ichii.w5x17"
+        "https://mangafire.to/manga/centuriaa.zxvjp"
+      ];
     };
   };
 }
