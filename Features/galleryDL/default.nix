acf5de5af630d2710ed83e73c25001dddddf337d
rPlakama <rPlakama@proton.me>
2026-05-05 10:10:18 -0300

    Added flareresolver

--- Features/galleryDL/default.nix
+++ Features/galleryDL/default.nix
@@ -5,6 +5,7 @@
   ...
 }:
 let
+
   cfg = config.optionals.features.galleryDl;
   user = config.core.user;
   downloadPath = "/media/mangas/download";
@@ -21,31 +22,16 @@ let
       sleep-chapter = "05.0-10.0";
       sleep-gallery = "15.0-35.0";
 
-      user-agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-      headers = {
-        Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
-        Accept-Language = "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7";
-        Accept-Encoding = "gzip, deflate, br";
-        "sec-ch-ua" = "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"";
-        "sec-ch-ua-mobile" = "?0";
-        "sec-ch-ua-platform" = "\"Windows\"";
-        "sec-fetch-dest" = "document";
-        "sec-fetch-mode" = "navigate";
-        "sec-fetch-site" = "none";
-        "sec-fetch-user" = "?1";
-        "upgrade-insecure-requests" = "1";
-        "priority" = "u=0, i";
-        DNT = "1";
-      };
       postprocessors = [
         {
           name = "zip";
-          extension = "cbz"; # Better accepted by Kavita
+          extension = "cbz";
           mode = "after";
         }
       ];
       mangafire = {
         lang = "pt-br";
+        flaresolverr = "http://127.0.0.1:8191/v1";
       };
     };
     downloader = {
@@ -84,6 +70,11 @@ in
 
     systemd.services.gallery-dl = {
       description = "gallery-dl Manga Downloader (Stealth Mode)";
+      after = [
+        "network.target"
+        "flaresolverr.service"
+      ];
+      wants = [ "flaresolverr.service" ];
       path = with pkgs; [
         p7zip
         zip
