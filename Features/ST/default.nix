639f8ae10bf1eeaa0fb369d2eedfef2fc260062b
rPlakama <rPlakama@proton.me>
2026-05-06 12:34:17 -0300

    Simplified ST module

--- Features/ST/default.nix
+++ Features/ST/default.nix
@@ -27,28 +27,16 @@ in
     default = false;
     description = "ST, AI RPG Service";
   };
+
   config = lib.mkIf config.optionals.features.ST.enable {
     services.sillytavern = {
       enable = true;
       port = 6720;
       configFile = configSrc;
     };
+
     systemd.services.sillytavern.serviceConfig.ExecStartPre =
       "${pkgs.bash}/bin/bash -c 'cp --remove-destination ${configSrc} /var/lib/SillyTavern/config.yaml && chmod 600 /var/lib/SillyTavern/config.yaml'";
-    security.acme.certs."moontier.online".extraDomainNames = [ "st.moontier.online" ];
-    services.nginx.virtualHosts."st.moontier.online" = {
-      useACMEHost = "moontier.online";
-      forceSSL = true;
-      extraConfig = ''
-        allow 192.168.1.0/24;
-        allow 100.64.0.0/10;
-        allow 127.0.0.1;
-        deny all;
-      '';
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:6720";
-        proxyWebsockets = true;
-      };
-    };
+    optionals.features.nginx.proxyServices.st = 6720;
   };
 }
