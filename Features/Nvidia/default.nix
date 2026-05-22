d32c8d57a2bc0b3ab4039fb67152327bcbf4e321
rPlakama <rPlakama@proton.me>
2026-05-21 23:13:35 -0300

    Updated on nvidia

--- Features/Nvidia/default.nix
+++ Features/Nvidia/default.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 let
-  cfg = config.optionals.features.nvidia;
+  cfg = config.core.features.nvidia;
 in
 {
   options.core.features.nvidia.enable = lib.mkOption {
@@ -20,7 +20,7 @@ in
         modesetting.enable = true;
         powerManagement.enable = false;
         open = false;
-        package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
     };
   };
