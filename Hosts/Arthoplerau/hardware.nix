2c4661a894b7e044bb2ba6504d4202c663917d45
rPlakama <rPlakama@proton.me>
2026-06-14 08:45:00 -0300

    Features/Noctalia: updt input

--- Hosts/Arthoplerau/hardware.nix
+++ Hosts/Arthoplerau/hardware.nix
@@ -21,28 +21,34 @@
 
   boot.kernelModules = [ "kvm-amd" ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  services.tlp = {
-    enable = true;
-    settings = {
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+  services.tlp = {
+    enable = false;
+    settings = {
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-      CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
 
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "balanced";
-      PLATFORM_PROFILE_ON_SAV = "low-power";
 
-      STOP_CHARGE_THRESH_BAT0 = 1;
-      SOUND_POWER_SAVE_ON_AC = 1;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+
+      SOUND_POWER_SAVE_ON_AC = 0;
       SOUND_POWER_SAVE_ON_BAT = 1;
       SOUND_POWER_SAVE_CONTROLLER = "Y";
 
       WIFI_PWR_ON_AC = "off";
       WIFI_PWR_ON_BAT = "on";
+
+      SCHED_POWERSAVE_ON_AC = 0;
+      SCHED_POWERSAVE_ON_BAT = 1;
+      NMI_WATCHDOG = 0;
+      WOL_DISABLE = "Y";
+      USB_AUTOSUSPEND = 1;
+      PCIE_ASPM_ON_AC = "performance";
+      PCIE_ASPM_ON_BAT = "powersave";
     };
   };
-
   hardware = {
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     firmware = [ pkgs.sof-firmware ];
