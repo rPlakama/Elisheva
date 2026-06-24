4b456cdb9864fc2d2324e7c1f7e9833ead16d20b
rPlakama <rPlakama@proton.me>
2026-06-24 10:23:20 -0300

    Features/core/bootloader.nix: Add plymouth option

--- Features/Core/bootloader.nix
+++ Features/Core/bootloader.nix
@@ -3,12 +3,10 @@
   lib,
   ...
 }:
-
 let
   cfg = config.features.bootloader;
   nixosInit = config.features.bootloader.nixos-init.enable;
 in
-
 {
   options.features.bootloader = {
     enable = lib.mkOption {
@@ -16,11 +14,15 @@ in
       default = true;
       description = "Bootloader Configuration";
     };
-
     nixos-init.enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
       description = "nixos-init";
+    };
+    plymouth.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Plymouth boot splash";
     };
   };
 
@@ -37,6 +39,16 @@ in
         systemd-boot.enable = true;
         timeout = 0;
       };
+      plymouth = lib.mkIf cfg.plymouth.enable {
+        enable = true;
+        theme = "bgrt";
+      };
+      kernelParams = lib.mkIf cfg.plymouth.enable [
+        "quiet"
+        "splash"
+        "udev.log_level=3"
+      ];
+      initrd.systemd.enable = true;
     };
   };
 }
