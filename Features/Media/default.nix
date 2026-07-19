d2fbcbfe8a07fd1cb2d6e46783b9b40ff3ee3661
rPlakama <rPlakama@proton.me>
2026-07-19 08:20:47 -0300

    Media packages declaration for manipulation

--- Features/Media/default.nix
+++ Features/Media/default.nix
@@ -1,9 +1,17 @@
-{lib, ...}: {
+{ pkgs, lib, ... }: {
   imports = lib.mapAttrsToList (name: _: ./${name}) (
     lib.filterAttrs (
       name: type:
-        (type == "directory")
-        || (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
+      (type == "directory") || (type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix")
     ) (builtins.readDir ./.)
   );
+
+  environment.systemPackages = with pkgs; [
+    exiftool
+    ffmpeg-full
+    opustags
+    opus-tools
+    opustags
+    flac2all
+  ];
 }
