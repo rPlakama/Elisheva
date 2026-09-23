b734a41701775cf15c304329f9b38510a2576f12
rPlakama <rPlakama@proton.me>
2026-09-23 18:36:07 -0300

    Simplify slskd cfg

--- Features/Media/slskd.nix
+++ Features/Media/slskd.nix
@@ -3,34 +3,37 @@
   config,
   pkgsM,
   ...
-}: let
+}:
+let
   featureCall = config.features;
   slskdPort = 5030;
   slskdListenPort = 50000;
-in {
+  secret = config.sops.placeholder;
+in
+{
   options.features.slskd.enable = lib.mkEnableOption "Soulseek client";
 
   config = lib.mkIf featureCall.slskd.enable {
     sops = {
       secrets = {
-        "slskd/username" = {};
-        "slskd/password" = {};
-        "slskd/web-username" = {};
-        "slskd/web-password" = {};
-        "slskd/api-main" = {};
+        "slskd/username" = { };
+        "slskd/password" = { };
+        "slskd/web-username" = { };
+        "slskd/web-password" = { };
+        "slskd/api-main" = { };
       };
       templates."slskd.env".content = ''
-        SLSKD_SLSK_USERNAME=${config.sops.placeholder."slskd/username"}
-        SLSKD_SLSK_PASSWORD=${config.sops.placeholder."slskd/password"}
-        SLSKD_USERNAME=${config.sops.placeholder."slskd/web-username"}
-        SLSKD_PASSWORD=${config.sops.placeholder."slskd/web-password"}
-        SLSKD_API_KEY=${config.sops.placeholder."slskd/api-main"}
+        SLSKD_SLSK_USERNAME=${secret."slskd/username"}
+        SLSKD_SLSK_PASSWORD=${secret."slskd/password"}
+        SLSKD_USERNAME=${secret."slskd/web-username"}
+        SLSKD_PASSWORD=${secret."slskd/web-password"}
+        SLSKD_API_KEY=${secret."slskd/api-main"}
       '';
     };
 
     features = {
       mediaPermissions.enable = true;
-      preservation.system.directories = ["/var/lib/slskd"];
+      preservation.system.directories = [ "/var/lib/slskd" ];
       unifiedDNS.proxyServices.slskd = {
         port = slskdPort;
         icon = "sh-slskd";
@@ -38,7 +41,7 @@ in {
       };
     };
 
-    networking.firewall.allowedTCPPorts = [slskdListenPort];
+    networking.firewall.allowedTCPPorts = [ slskdListenPort ];
 
     services.slskd = {
       enable = true;
